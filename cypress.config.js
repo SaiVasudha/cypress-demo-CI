a8ecba32a6d5284d6ce7247d6b11d77407ed5e2f
saivasudha <sai.vasudha@codecraft.co.in>
2024-03-26 11:04:56 +0530

    feat: automated userlogin with correct and wrong credentials of conti plus project

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require("cypress");
+require("dotenv").config();
+
+module.exports = defineConfig({
+  projectId: "hky1n6",
+  env: { ...process.env },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+  viewportHeight: 960,
+  viewportWidth: 1536,
+  defaultCommandTimeout: 20000,
+  pageLoadTimeout: 90000,
+});
